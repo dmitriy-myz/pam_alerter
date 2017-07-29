d76de527241c25781af03b364a3ebeb41486af36
Dmitriy Muzyka <a5b1ca@gmail.com>
2017-07-29 20:18:41 +0300

    white ips

--- alerter.py
+++ alerter.py
@@ -23,14 +23,14 @@ if not os.path.isdir(store_dir):
 def load_settings(filename):
     try:
         with open(filename, 'r') as f:
-            settings = json.load(f)
+            _settings = json.load(f)
     except:
-        settings = []
-    return settings
+        _settings = []
+    return _settings
 
-def save_settings(filename, settings):
+def save_settings(filename, _settings):
     with open(filename, 'w') as f:
-        json.dump(settings, f, indent=4, sort_keys=True)
+        json.dump(_settings, f, indent=4, sort_keys=True)
 
 def send_email(message):
     msg = MIMEText(message, 'plain')
@@ -42,12 +42,12 @@ def send_email(message):
 
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
-mail_settings_file = os.path.join(script_directory, settings_file)
+settings_file = os.path.join(script_directory, settings_file)
 
-mail_settings = load_settings(mail_settings_file)
-sender = mail_settings['sender']
-receivers = mail_settings['receivers']
-
+settings = load_settings(settings_file)
+sender = settings['sender']
+receivers = settings['receivers']
+white_ips = settings['white_ips']
 
 # security check
 if user != os.path.basename(user):
@@ -60,10 +60,12 @@ ip_list_per_user = os.path.join(store_dir, user)
 known_ips = load_settings(ip_list_per_user)
 
 
+
 if pam_type != 'close_session':
     if host not in known_ips:
-        message = 'Login by: %s from: %s\n\nservice = %s pam_type = %s' % (user, host, service, pam_type)
-        send_email(message)
         known_ips.append(host)
         save_settings(ip_list_per_user, known_ips)
+        if host not in white_ips:
+            message = 'Login by: %s from: %s\n\nservice = %s pam_type = %s' % (user, host, service, pam_type)
+            send_email(message)
 
